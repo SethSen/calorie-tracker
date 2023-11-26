b735fc8d4bbb9250d6f6789922976c4f0c92f2b1
seth sen <seth.sen@hotmail.com>
2023-11-25 18:22:41 -0800

    fix

--- Processing/app.py
+++ Processing/app.py
@@ -9,34 +9,43 @@ import json
 import os
 from flask_cors import CORS, cross_origin
 
-# Load application configuration and logging
+# Load application configuration from 'app_conf.yml'
 with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
 
+# Load logging configuration from 'log_conf.yml'
 with open('log_conf.yml', 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
+# Initialize logger
 logger = logging.getLogger('basicLogger')
 
 def get_stats():
     logger.info("GET /stats request started")
     filename = app_config['datastore']['filename']
 
-    if os.path.isfile(filename):
+    try:
         with open(filename, 'r') as f:
             current_stats = json.load(f)
-        return current_stats, 200
-    else:
+    except FileNotFoundError:
         logger.error("Statistics file not found")
         return {"message": "Statistics do not exist"}, 404
 
+    logger.debug(f"Statistics: {current_stats}")
+    logger.info("GET /stats request completed")
+    return current_stats, 200
+
+
 def populate_stats():
     logger.info("Start Periodic Processing")
-
+    
     filename = app_config['datastore']['filename']
-    if not os.path.isfile(filename):
-        logger.error("Statistics file not found. Using default stats.")
+    try:
+        with open(filename, 'r') as f:
+            current_stats = json.load(f)
+    except FileNotFoundError:
+        logger.error("Statistics file not found")
         current_stats = {
             'num_users': 0,
             'max_age': 0,
@@ -45,49 +54,63 @@ def populate_stats():
             'max_calories': 0,
             'last_updated': "2000-01-01T00:00:00Z"
         }
-    else:
-        with open(filename, 'r') as f:
-            current_stats = json.load(f)
 
-    timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
+    now = datetime.datetime.now()
+    timestamp = now.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    personal_info_url = f"{app_config['eventstore']['url']}/personal-info?timestamp={current_stats['last_updated']}&end_timestamp={timestamp}"
+    food_log_url = f"{app_config['eventstore']['url']}/food-log?timestamp={current_stats['last_updated']}&end_timestamp={timestamp}"
 
     try:
-        response_users = requests.get(f"{app_config['eventstore']['url']}/personal-info", params={'timestamp': current_stats['last_updated']})
-        response_food_logs = requests.get(f"{app_config['eventstore']['url']}/food-log", params={'timestamp': current_stats['last_updated']})
+        personal_info_response = requests.get(personal_info_url)
+        food_log_response = requests.get(food_log_url)
 
-        if response_users.status_code == 200 and response_food_logs.status_code == 200:
-            data_users = response_users.json()
-            data_food_logs = response_food_logs.json()
-
-            # Calculate new statistics
-            max_age = max([current_stats['max_age']] + [int(u['age']) for u in data_users])
-            max_weight = max([current_stats['max_weight']] + [int(u['weight']) for u in data_users])
-            max_calories = max([current_stats['max_calories']] + [int(f['calories']) for f in data_food_logs])
-
-            updated_data = {
-                'num_users': current_stats['num_users'] + len(data_users),
-                'max_age': max_age,
-                'max_weight': max_weight,
-                'num_food_log': current_stats['num_food_log'] + len(data_food_logs),
-                'max_calories': max_calories,
-                'last_updated': timestamp
-            }
-
-            # Write updated data to the file
-            with open(filename, 'w') as f:
-                json.dump(updated_data, f, indent=4)
-            logger.debug(f"Updated data: {updated_data}")
-
+        if personal_info_response.status_code == 200:
+            info_events = personal_info_response.json()
+            logger.info(f"Received {len(info_events)} new personal info events")
         else:
-            logger.error(f"Failed to retrieve data from APIs. Status: Users - {response_users.status_code}, Food Logs - {response_food_logs.status_code}")
+            logger.error("Failed to retrieve personal info events")
+            info_events = []
 
-    except Exception as e:
-        logger.error(f"Error during processing: {e}")
+        if food_log_response.status_code == 200:
+            food_log_events = food_log_response.json()
+            logger.info(f"Received {len(food_log_events)} new food log events")
+        else:
+            logger.error("Failed to retrieve food log events")
+            food_log_events = []
 
-    logger.info("Periodic Processing Ended")
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Request error: {e}")
+        info_events = []
+        food_log_events = []
 
+    for event in info_events:
+        current_stats['num_users'] += 1
+        current_stats['max_age'] = max(current_stats['max_age'], event.get('age', 0))
+        current_stats['max_weight'] = max(current_stats['max_weight'], event.get('weight', 0))
+
+    for event in food_log_events:
+        current_stats['num_food_log'] += 1
+        current_stats['max_calories'] = max(current_stats['max_calories'], event.get('calories', 0))
+
+    current_stats['last_updated'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    with open(filename, 'w') as f:
+        json.dump(current_stats, f, indent=4)
+
+    logger.info("Finished Processing")
 
 def init_scheduler():
     sched = BackgroundScheduler(daemon=True)
     sched.add_job(populate_stats, 'interval', seconds=app_config['scheduler']['period_sec'])
-    sched.start
+    sched.start()
+
+# Initialize and configure the Connexion application
+app = connexion.FlaskApp(__name__, specification_dir='')
+CORS(app.app)
+app.app.config['CORS_HEADERS'] = 'Content-Type'
+app.add_api('calorie-tracker.yml', strict_validation=True, validate_responses=True)
+
+if __name__ == "__main__":
+    init_scheduler()
+    app.run(port=8100, debug=True, use_reloader=False)
