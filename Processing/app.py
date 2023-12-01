d7524c3cdc82f9a5ceee658a115067b5c36abc07
seth sen <seth.sen@hotmail.com>
2023-11-30 16:07:22 -0800

    Lab11 V5

--- Processing/app.py
+++ Processing/app.py
@@ -123,7 +123,7 @@ def init_scheduler():
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api('calorie-tracker',base_path="/processing",strict_validation=True, validate_responses=True )
+app.add_api('calorie-tracker.yml',base_path="/processing",strict_validation=True, validate_responses=True )
 if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
     CORS(app.app)
     app.app.config['CORS_HEADERS'] = 'Content-Type'
