9d5631f5321217144e27a0171d591d10dba8d9ee
seth sen <seth.sen@hotmail.com>
2023-11-30 21:13:02 -0800

    lab11 v18

--- Storage/create_tables_mysql.py
+++ Storage/create_tables_mysql.py
@@ -1,26 +1,26 @@
-import pymysql
+import mysql.connector
 import yaml
 
 # Connect to the MySQL server
+# conn = mysql.connector.connect(host='localhost', user='root', password='password', database='calorie_tracker')
 
 with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-# Database connection parameters
 user = app_config['datastore']['user']
 password = app_config['datastore']['password']
 hostname = app_config['datastore']['hostname']
 port = app_config['datastore']['port']
 db = app_config['datastore']['db']
 
-# Connect to the MySQL server
-db_conn = pymysql.connect(
+
+db_conn = mysql.connector.connect(
     host=hostname,
     user=user,
     password=password,
     database=db,
-    port=port
-)
+    port=port,
+    auth_plugin='mysql_native_password')
 
 db_cursor = db_conn.cursor()
 
