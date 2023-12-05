091a3c788f0d04d1c033af8b3aa94bd8d176a2a5
seth sen <seth.sen@hotmail.com>
2023-12-05 14:21:15 -0800

    now fixed i think

--- Health/app.py
+++ Health/app.py
@@ -121,13 +121,18 @@ def init_scheduler():
                   seconds=app_config['scheduler']['period_sec'])
     sched.start()
 
+
+# app = connexion.FlaskApp(__name__, specification_dir='')
+# app.add_api('openapi.yml',base_path="/health",strict_validation=True, validate_responses=True )
+# if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
+#     CORS(app.app)
+#     app.app.config['CORS_HEADERS'] = 'Content-Type'
+
+# Initialize Connexion app to integrate Flask with Swagger
 app = connexion.FlaskApp(__name__, specification_dir='')
-CORS(app.app)
-app.app.config['CORS_HEADERS'] = 'Content-Type'
-app.add_api("openapi.yml",
-            strict_validation = True,
-            validate_responses=True)
+# Add API with validation options
+app.add_api("openapi.yml", base_path="/health", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
     init_scheduler()
-    app.run(port=8120, debug = True, use_reloader=False)
+    app.run(port=8120)
